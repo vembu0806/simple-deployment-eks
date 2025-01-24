b24cfe80f7c42f52ea865007399317b6d202bd32
Vembarasan Thambidurai <37688145+vembu0806@users.noreply.github.com>
2025-01-24 09:51:03 +0530

    commit eks.tf

---
+++ eks.tf
@@ -0,0 +1,17 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.26"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  node_groups = {
+    default = {
+      desired_capacity = var.desired_capacity
+      max_size         = 3
+      min_size         = 1
+      instance_type    = var.node_instance_type
+    }
+  }
+}
